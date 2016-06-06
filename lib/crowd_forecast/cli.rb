2b2d120ad1cc1c91777b909cc682d242ec48265e
Christian Whitesides <whitesidescr@gmail.com>
2016-06-06 15:54:33 -0600

    Refactor cli, park and scraper classes

--- lib/crowd_forecast/cli.rb
+++ lib/crowd_forecast/cli.rb
@@ -5,40 +5,39 @@ class CrowdForecast::CLI
   end
 
   def list_parks
-    CrowdForecast::Park.all
-    @parks.each.with_index(1) do |park, i|
+    CrowdForecast::Park.all.each.with_index(1) do |park, i|
       puts "#{i}. #{park.name} - #{park.current_status}"
     end
   end
 
-  def print_5_day_forecast(park_number) 
-    if @parks[park_number-1].next_5_days.empty?
+  def print_5_day_forecast(park)
+    if park.next_5_days.empty?
       puts "        Information not available.        "
     else
-      @parks[park_number-1].next_5_days.each do |d|
+      park.next_5_days.each do |d|
         puts "       " + d + "       "
       end
     end
   end
 
-  def print_calendar_notes(park_number)
-    if @parks[park_number-1].calendar_notes == ""
+  def print_calendar_notes(park)
+    if park.calendar_notes == ""
       puts "There are no major calendar items to plan around."
     else
       puts "* Important Calendar Items To Keep In Mind *"
       puts ""
-      puts "#{@parks[park_number-1].calendar_notes}"
+      puts "#{park.calendar_notes}"
     end
   end
 
-  def print_park(park_number)
-    puts "*------------* #{@parks[park_number-1].name} *------------*"
+  def print_park(park)
+    puts "*------------* #{park.name} *------------*"
     puts ""
     puts "            Five-Day Forecast            "
-    print_5_day_forecast(park_number)
+    print_5_day_forecast(park)
     puts ""
     puts ""
-    print_calendar_notes(park_number)
+    print_calendar_notes(park)
     puts ""
   end
 
@@ -54,18 +53,20 @@ class CrowdForecast::CLI
     while input != "exit"
       puts "Enter 1-7 to look at the 5-day forecast of a park." unless input == "exit"
       input = gets.strip.downcase
+
+      park = CrowdForecast::Park.find(input.to_i)
       puts ""
       puts ""
 
       if (1..7).include?(input.to_i)
-        print_park(input.to_i)
+        print_park(park)
       elsif input == "list"
         list_parks
       else
         other_result = <<-DOC.gsub /^\s+/, ""
         That's not a valid request.
         For a full list of parks enter "list"
-        To exit, just enter "exit"
+        To exit enter "exit"
         DOC
         puts other_result unless input == "list" || input == "exit"
       end
--- lib/crowd_forecast/park.rb
+++ lib/crowd_forecast/park.rb
@@ -1,65 +1,18 @@
 class CrowdForecast::Park
   attr_accessor :name, :current_status, :next_5_days, :calendar_notes
 
-  # @@all = []
+  def initialize(name = nil, current_status = nil, next_5_days = nil, calendar_notes = nil)
+    @name = name
+    @current_status = current_status
+    @next_5_days = next_5_days
+    @calendar_notes = calendar_notes
+  end
 
   def self.all
-    @@all ||= scrape_parks
+    @@all ||= CrowdForecast::Scraper.scrape_parks
   end
 
-
-  def self.scrape_parks
-    park_urls = [
-    "disneyland/", 
-    "dca-disney-california-adventure/",
-    "knotts-berry-farm/",
-    "six-flags-magic-mountain/",
-    "universal-studios-hollywood/",
-    "seaworld-san-diego/",
-    "legoland-california/"
-    ]
-
-    park_urls.map do |slug|
-      doc = Nokogiri::HTML(open("http://www.isitpacked.com/live-crowd-trackers/" + slug))
-
-      park = self.new
-      park.name = doc.css('h1.page-title').text.gsub(' Crowd Tracker', '')
-      park.current_status = ""
-      park.next_5_days = self.scrape_5_day_forecast(slug)
-      park.calendar_notes = self.scrape_calendar_notes(slug)
-
-      status_options = doc.css('div.entry-content div a.button')
-      status_options.each do |status|
-        if status.attr('style').include?("color:#fff")
-          park.current_status = status.children.text
-        end
-      end
-      park
-    end
+  def self.find(id)
+    self.all[id-1]
   end
-
-  def self.scrape_5_day_forecast(slug)
-    doc = Nokogiri::HTML(open("http://www.isitpacked.com/live-crowd-trackers/" + slug))
-
-    crowd_forecast_list = doc.css('.supe-item-holder div.rhc-widget-upcoming-item')
-
-    next_5_days = crowd_forecast_list.map.with_index do |day, i|
-      month = crowd_forecast_list[i].css('.rhc-date-month-year').text
-      day = crowd_forecast_list[i].css('.rhc-date-day').text
-      status = crowd_forecast_list[i].css('.rhc-widget-upcoming-title a').text
-      "#{month} #{day} -.- #{status}"
-    end
-    next_5_days
-  end
-
-
-  def self.scrape_calendar_notes(slug)
-    if slug == "disneyland/" || slug == "dca-disney-california-adventure/"
-      doc = Nokogiri::HTML(open("http://www.isitpacked.com/disneyland-crowd-forecast-predictor-calendar/"))
-    else
-      doc = Nokogiri::HTML(open("http://www.isitpacked.com/crowd-calendars/" + slug))
-    end
-    doc.css('div#aone p').text.gsub(/For real.+Tracker./, "")
-  end
-
 end
